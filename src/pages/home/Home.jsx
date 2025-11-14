1eefad67a957b28290d3369d6980c589a3830364
leenaabdalrahman11 <leenasba02@gmail.com>
2025-11-14 14:14:34 +0200

    feat: design home page with hero image

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,17 @@
-import { Typography } from '@mui/material'
+import { Container, Typography } from '@mui/material'
 import style from './Home.module.css'
 import React from 'react'
+import Categories from '../categories/Categories'
 
 export default function Home() {
   return (
-    <div>
-        <Typography  component={'h1'} variant='body1'>Hello </Typography>
-        <Typography component={'p'} variant='subtitle1'>this is home</Typography>
+    <>
+      <div className={style.HeaderImage}></div>
+              <Container maxWidth='lg'>
+                        <Categories />
         <Typography href='#' component={'a'} variant='subtitle2'>Click</Typography>
-    </div>
+              </Container>
+
+    </>
   )
 }
