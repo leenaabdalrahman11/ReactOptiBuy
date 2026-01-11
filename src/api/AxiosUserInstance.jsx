fab2cc61a8570ed7ae4457dc7270b40e0e7c530d
leenaabdalrahman11 <leenasba02@gmail.com>
2026-01-11 20:35:12 +0200

    define AxiosUserInstance

--- src/api/AxiosUserInstance.jsx
+++ src/api/AxiosUserInstance.jsx
@@ -1,17 +1,30 @@
 import axios from "axios";
 
 const AxiosUserInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
+const getSessionId = () => {
+  let sid = localStorage.getItem("sessionId");
+  if (!sid) {
+    sid = "guest_" + Date.now() + "_" + Math.random().toString(36).substring(2);
+    localStorage.setItem("sessionId", sid);
+  }
+  return sid;
+};
+
 AxiosUserInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("userToken");
-  const sid = localStorage.getItem("sessionId");
+  const sid = localStorage.getItem("sessionId") || getSessionId();
 
   config.headers = config.headers || {};
-  if (sid) config.headers["x-session-id"] = sid;
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  config.headers["x-session-id"] = sid;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
   return config;
 });
