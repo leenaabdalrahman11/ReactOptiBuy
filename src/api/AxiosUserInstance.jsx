81b542a393328630ab87f048c7a19bb0b04eab23
leenaabdalrahman11 <leenasba02@gmail.com>
2026-01-11 20:31:53 +0200

    AxiosUserInstance

--- src/api/AxiosUserInstance.jsx
+++ src/api/AxiosUserInstance.jsx
@@ -1,18 +1,19 @@
 import axios from "axios";
 
 const AxiosUserInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL, 
+  withCredentials: true,
 });
 
-AxiosUserInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("userToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+AxiosUserInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("userToken");
+  const sid = localStorage.getItem("sessionId");
+
+  config.headers = config.headers || {};
+  if (sid) config.headers["x-session-id"] = sid;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  return config;
+});
 
 export default AxiosUserInstance;
