b3802b16df19a818eb9b33ff9beeda85d889b727
leenaabdalrahman11 <leenasba02@gmail.com>
2026-01-11 20:36:56 +0200

    define AxiosUserInstance

--- src/api/AxiosUserInstance.jsx
+++ src/api/AxiosUserInstance.jsx
@@ -2,31 +2,17 @@ import axios from "axios";
 
 const AxiosUserInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
 });
 
-const getSessionId = () => {
-  let sid = localStorage.getItem("sessionId");
-  if (!sid) {
-    sid = "guest_" + Date.now() + "_" + Math.random().toString(36).substring(2);
-    localStorage.setItem("sessionId", sid);
-  }
-  return sid;
-};
-
-AxiosUserInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("userToken");
-  const sid = localStorage.getItem("sessionId") || getSessionId();
-
-  config.headers = config.headers || {};
-
-  config.headers["x-session-id"] = sid;
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
+AxiosUserInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("userToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default AxiosUserInstance;
